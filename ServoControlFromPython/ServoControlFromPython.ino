2cea84b2f666486c2db58591f472fd664e93a2d9
Yanai99 <yanaiparan@gmail.com>
2023-05-04 10:15:49 +0300

    arduino fix

--- ServoControlFromPython/ServoControlFromPython.ino
+++ ServoControlFromPython/ServoControlFromPython.ino
@@ -3,26 +3,32 @@
 Servo myservo;  // create servo object to control a servo
 int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
-int timeout = 0; // timeout value received from Python
-bool turnVolume = false;
+int pythonMsg = 0; // timeout value received from Python
 
 void setup() {
   Serial.begin(9600); // Initialize serial communication
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
   myservo.write(0);
+  digitalWrite(13, HIGH); // turn on LED
 }
 
 void loop() {
   if (Serial.available() > 0) { // If data is available on serial port
-    timeout = Serial.parseInt(); // Read the timeout value from Python
-    digitalWrite(13, HIGH); // turn on LED
-    turnVolume = true;
-  }
-  if (turnVolume) {
-    for (val = 0; val <= 180; val++) { // move from left to right
+    pythonMsg = Serial.parseInt(); // Read the timeout value from Python
+
+    if(pythonMsg == 1)
+    {
+      digitalWrite(13, LOW); // turn on LED
+      digitalWrite(12, HIGH); // turn on LED
+    }
+    else if(pythonMsg == 2)
+    {
+      digitalWrite(12, LOW); // turn on LED
+      digitalWrite(8, HIGH); // turn on LED
+       for (val = 0; val <= 180; val++) { // move from left to right
       myservo.write(val);
       delay(300);
     }
-    turnVolume = false; // reset the flag after the servo movement is complete
+    }
   }
 }
--- main.py
+++ main.py
@@ -5,17 +5,22 @@ import pyautogui
 import time
 import serial
 
+def seconds_to_hours_minutes(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return int(hours), int(minutes)
+
+
 ser = serial.Serial('COM5', 9600)
 
-ready = 0
 activate = 1
 wake_up = 2
 
 # Define the list of URLs to choose from
 urls = {
     '1': ('https://tunein.com/radio/Joint-Radio-Blues-s203399/', 'Joint Radio Blues'),
-    '2': ('https://radio.coolsite.co.il/m.php?radio=6', 'Galey Tzahal'),
-    '3': ('https://radio.coolsite.co.il/m.php?radio=1', 'Eco-88')
+    '2': ('https://tunein.com/radio/KAN-88-880-s100340/', 'Kan 88'),
+    '3': ('https://tunein.com/radio/Juice-Belfast-1038-s297336/', 'Juice Belfast')
 }
 
 # Ask the user for the time and URL choice
@@ -30,18 +35,20 @@ print("Choose a Station:")
 for key, value in urls.items():
     print(f"{key}. {value[1]}")
 url_choice = input()
+ser.write(str(activate).encode())
 
 # Calculate the time delay to the alarm_time
 time_diff = alarm_time - datetime.datetime.now()
 time_delay = time_diff.total_seconds()
 #parameter to send to activate arduino
 
+hours, minutes = seconds_to_hours_minutes(time_delay)
+print(f"we'll wake you up in {hours} hours and {minutes} minutes")
 
-print(time_delay)
 # Wait for the time delay and open the URL
 if(time_delay > 0):
     time.sleep(time_delay)
 
-ser.write(str(activate).encode())
+ser.write(str(wake_up).encode())
 # Open the URL and click the "Play" button
 webbrowser.open(urls[url_choice][0])
